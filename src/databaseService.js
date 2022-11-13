091e25709dd6d02ef1de72fda7f60a9cd337cde1
Geordous-Huxwell <jconl266@mtroyal.ca>
2022-11-12 20:17:37 -0700

    item methods for DBservice

--- src/databaseService.js
+++ src/databaseService.js
@@ -1,21 +1,102 @@
 const sqlite3 = require('sqlite3').verbose();
 const database = new sqlite3.Database('./src/backend_database.db', (err) => {
-  console.log(err ? err.message : "Connected to database!");
+    console.log(err ? err.message : "Connected to database!");
 });
 /**Dummy Data...*/
 
-database.run(`INSERT OR IGNORE INTO supplier(id, name, address, contact) VALUES (?, ?, ?, ?)`, [1, "Jeff", "123 Happy Street", "Do Not Contact"], function(err) {
-  console.log(err ? err.message : "Added Supplier row ${this.lastID} to dattabase!");
-});
+database.run(`INSERT OR IGNORE INTO supplier(id, name, address, contact) VALUES (?, ?, ?, ?)`, [1, "Jeff", "123 Happy Street", "Do Not Contact"],
+    function(err) {
+        console.log(err ? err.message : "Added Supplier row ${this.lastID} to dattabase!");
+    });
 
 /**Code goes here!*/
 
+async function ItemExistsById(id) {
+    if (isNaN(id)) {
+        return new Promise((res, rej) => { rej("-> Not A Number!") });
+    } else {
+        let sql = `SELECT Id id,
+                  Name name,
+                  Price price,
+                  Quantity quantity,
+                  Supplier_id supplier_id
+              FROM item
+              WHERE Id = (?)
+              ORDER BY Id`;
+
+        return runQuery(sql, [id]);
+    };
+};
+
+async function ItemExistsByName(name) {
+    if (!name) {
+        return new Promise((res, rej) => { rej("-> Not A String!") });
+    } else {
+        let sql = `SELECT Id id,
+                  Name name,
+                  Price price,
+                  Quantity quantity,
+                  Supplier_id supplier_id
+              FROM item
+              WHERE Name = (?)
+              ORDER BY Id`;
+
+        return runQuery(sql, [name])
+    };
+};
+
+async function GetAllItems() {
+    let sql = `SELECT Id id,
+                Name name,
+                Price price,
+                Quantity quantity,
+                Supplier_id supplier_id
+            FROM item
+            ORDER BY Id`;
+
+    return runQuery(sql, [])
+}
+
+async function AddItem(item) {
+    if (parseInt(item.id) == NaN) {
+        return new Promise((res, rej) => { rej("-> Provided Id is not a number.") });
+    }
+    let sql = `INSERT OR IGNORE INTO item(id,
+                      name,
+                      price,
+                      quantity,
+                      supplier_id)
+              VALUES (?, ?, ?, ?, ?)`;
+
+    return runChange(sql, [parseInt(item.id), item.name, item.price, item.quantity, item.supplier_id])
+}
+
+async function UpdateQuantity(itemId, quantity) {
+    if (parseInt(itemId) == NaN) {
+        return new Promise((res, rej) => { rej("-> Provided Id is not a number.") });
+    } else if (parseInt(quantity) == NaN) {
+        return new Promise((res, rej) => { rej("-> Provided quantity is not a number.") });
+    }
+
+    let sql = `UPDATE item
+          SET quantity = quantity + ${quantity}
+          WHERE id=${itemId}`;
+
+    return runChange(sql, []);
+
+}
+
+async function DeleteItem(itemId) {
+    let sql = `DELETE FROM item WHERE id=${itemId}`;
+
+    return runChange(sql, [])
+}
 
 async function SupplierExistsById(id) {
-  if (isNaN(id)) {
-    return new Promise((res, rej) => { rej("-> Not A Number!") });
-  } else {
-    let sql = `SELECT Id id,
+    if (isNaN(id)) {
+        return new Promise((res, rej) => { rej("-> Not A Number!") });
+    } else {
+        let sql = `SELECT Id id,
                   Name name,
                   Address address,
                   Contact contact
@@ -23,15 +104,17 @@ async function SupplierExistsById(id) {
               WHERE Id = (?)
               ORDER BY Id`;
 
-    return runQuery(sql, [id])
-  };
+        return runQuery(sql, [id])
+    };
 };
 
+
+
 async function SupplierExistsByName(name) {
-  if (!name) {
-    return new Promise((res, rej) => { rej("-> Not A String!") });
-  } else {
-    let sql = `SELECT Id id,
+    if (!name) {
+        return new Promise((res, rej) => { rej("-> Not A String!") });
+    } else {
+        let sql = `SELECT Id id,
                   Name name,
                   Address address,
                   Contact contact
@@ -39,67 +122,67 @@ async function SupplierExistsByName(name) {
               WHERE Name = (?)
               ORDER BY Id`;
 
-    return runQuery(sql, [name])
-  };
+        return runQuery(sql, [name])
+    };
 };
 
 async function GetAllSuppliers() {
-  let sql = `SELECT Id id,
+    let sql = `SELECT Id id,
                 Name name,
                 Address address,
                 Contact contact
             FROM supplier
             ORDER BY Id`;
 
-  return runQuery(sql, [])
+    return runQuery(sql, [])
 }
 
 async function AddSupplier(supplier) {
-  if(parseInt(supplier.id) == NaN) {
-    return new Promise((res, rej) => { rej("-> Provided Id is not a number.") });
-  }
-  let sql = `INSERT OR IGNORE INTO supplier(id,
+    if (parseInt(supplier.id) == NaN) {
+        return new Promise((res, rej) => { rej("-> Provided Id is not a number.") });
+    }
+    let sql = `INSERT OR IGNORE INTO supplier(id,
                       name,
                       address,
                       contact)
               VALUES (?, ?, ?, ?)`;
 
-  return runChange(sql, [parseInt(supplier.id), supplier.name, supplier.address, supplier.contact])
+    return runChange(sql, [parseInt(supplier.id), supplier.name, supplier.address, supplier.contact])
 }
 
 async function DeleteSupplier(supplierId) {
-  let sql = `DELETE FROM supplier WHERE id=${supplierId}`;
+    let sql = `DELETE FROM supplier WHERE id=${supplierId}`;
 
-  return runChange(sql, [])
+    return runChange(sql, [])
 }
 
 async function runQuery(sql, input) {
-  return new Promise((res, rej) => {
-    database.all(sql, input, (err, rows) => {
-      if (err) {
-        //console.log(err);
-        rej(err);
-      }
-      if (rows.length == 0) {
-        msg = "-> 0 rows found!"
-        //console.log(msg);
-        rej(msg);
-      }
-      console.log(rows);
-      res(rows);
+    return new Promise((res, rej) => {
+        database.all(sql, input, (err, rows) => {
+            if (err) {
+                //console.log(err);
+                rej(err);
+            }
+            if (rows.length == 0) {
+                msg = "-> 0 rows found!"
+                    //console.log(msg);
+                rej(msg);
+            }
+            console.log(rows);
+            res(rows);
+        });
     });
-  });
 };
 
 async function runChange(sql, input) {
-  return new Promise((res, rej) => {
-    database.run(sql, input, function(err) {
-      if (err) {
-        rej(err);
-      }
-      res("Database updated!");
+    return new Promise((res, rej) => {
+        database.run(sql, input, function(err) {
+            if (err) {
+                rej(err);
+            }
+            res("Database updated!");
+        });
     });
-  });
 }
 
 
@@ -113,4 +196,16 @@ async function runChange(sql, input) {
 //    console.log(err ? err.message : "Closed database!");
 //});
 
-module.exports = { SupplierExistsById, SupplierExistsByName, GetAllSuppliers, AddSupplier, DeleteSupplier }
+module.exports = {
+    ItemExistsById,
+    ItemExistsByName,
+    GetAllItems,
+    AddItem,
+    UpdateQuantity,
+    DeleteItem,
+    SupplierExistsById,
+    SupplierExistsByName,
+    GetAllSuppliers,
+    AddSupplier,
+    DeleteSupplier
+};
